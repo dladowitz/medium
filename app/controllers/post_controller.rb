53baf7c65124132f91abb955329c5db74cbbefb5
David Ladowitz <david@ladowitz.com>
2014-03-08 23:19:51 -0800

    created index page and connected links to show pages for postings

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,19 +2,45 @@ class PostController < ApplicationController
   MEDIUM_URL      = "https://medium.com/"
   CLOUDFRONT_URL  = "https://d262ilb51hltx0.cloudfront.net/fit/t/1600/672/"
   JSON_FORMAT     = "?format=json"
-
-  BUNCH_O_PAGES = ["p/2c0424c1d4bc", "race-class/b84d4011d17e", "human-parts/9f53ef6a1c10", "apple-daily/f5f8c807d868", "life-learning/2a1841f1335d"]
+  SPOILER         = "])}while(1);</x>"
 
   def index
-    spoiler = "])}while(1);</x>"
-    response = HTTParty.get(MEDIUM_URL + BUNCH_O_PAGES[[*0..4].sample] + JSON_FORMAT)
-    string_response = response.body.gsub(spoiler, "")
-    json_response = JSON.parse(string_response)
-    @title = json_response["payload"]["value"]["title"]
-    @main_image = CLOUDFRONT_URL + json_response["payload"]["value"]["content"]["image"]["imageId"]
-    @paragraphs = json_response["payload"]["value"]["content"]["bodyModel"]["paragraphs"].map{|block_info| block_info["text"]}
+    # Medium returns json with a spoiler prepended to it. Probably to prevent json hijacking.
+    dirty_response = HTTParty.get(MEDIUM_URL + JSON_FORMAT)
+    json_content   = get_valid_json(dirty_response)  
+
+    # all posts the page
+    @posts = json_content["payload"]["value"].map{ |post| post }
+    # list of associated collections posts on page are stored in
+    @collections = json_content["payload"]["references"]["Collection"].map{ |collection| collection }
   end
 
+
   def show
+    # Medium returns json with a spoiler prepended to it. Probably to prevent json hijacking.
+    post_url        = MEDIUM_URL + params[:collection_name] + "/" + params[:post_id] + JSON_FORMAT
+    dirty_response  = HTTParty.get(post_url)
+    json_content    = get_valid_json(dirty_response)     
+    
+    @title      = json_content["payload"]["value"]["title"]
+    @main_image = CLOUDFRONT_URL + json_content["payload"]["value"]["content"]["image"]["imageId"]
+    @paragraphs = json_content["payload"]["value"]["content"]["bodyModel"]["paragraphs"].map{|block_info| block_info["text"]}
   end
+
+  private
+
+  def get_valid_json(dirty_response)
+    clean_response = remove_spoiler(dirty_response)
+    convert_to_json(clean_response)
+  end
+    
+  def remove_spoiler(dirty_response)
+    dirty_response.body.gsub(SPOILER, "")
+  end
+
+  def convert_to_json(json_string)
+    JSON.parse(json_string)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 MediumReader::Application.routes.draw do
+  root "post#index" 
+
   get "post/index"
   get "post/show"
-  get "index/show"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
