50af29ceb7fa8e348b2d4e30da0338db71b02d51
David Ladowitz <david@ladowitz.com>
2014-03-09 21:17:49 -0700

    added author info and helper methods

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,6 +1,6 @@
 class PostController < ApplicationController
-  MEDIUM_URL      = "https://medium.com/"
-  CLOUDFRONT_URL  = "https://d262ilb51hltx0.cloudfront.net/fit/t/1600/672/"
+  MEDIUM_URL      = "https://medium.com"
+  CLOUDFRONT_URL  = "https://d262ilb51hltx0.cloudfront.net"
   JSON_FORMAT     = "?format=json"
   SPOILER         = "])}while(1);</x>"
 
@@ -13,17 +13,18 @@ class PostController < ApplicationController
     @posts = json_content["payload"]["value"].map{ |post| post }
     # list of associated collections posts on page are stored in
     @collections = json_content["payload"]["references"]["Collection"].map{ |collection| collection }
+    @authors = json_content["payload"]["references"]["User"].map{ |author| author }
   end
 
 
   def show
     # Medium returns json with a spoiler prepended to it. Probably to prevent json hijacking.
-    post_url        = MEDIUM_URL + params[:collection_name] + "/" + params[:post_id] + JSON_FORMAT
+    post_url        = "#{MEDIUM_URL}/#{params[:collection_name]}/#{params[:post_id]}#{JSON_FORMAT}"
     dirty_response  = HTTParty.get(post_url)
     json_content    = get_valid_json(dirty_response)     
     
     @title      = json_content["payload"]["value"]["title"]
-    @main_image = CLOUDFRONT_URL + json_content["payload"]["value"]["content"]["image"]["imageId"]
+    @main_image = "#{CLOUDFRONT_URL}/fit/t/1600/672/#{json_content["payload"]["value"]["content"]["image"]["imageId"]}"
     @paragraphs = json_content["payload"]["value"]["content"]["bodyModel"]["paragraphs"].map{|block_info| block_info["text"]}
   end
 
@@ -41,6 +42,4 @@ class PostController < ApplicationController
   def convert_to_json(json_string)
     JSON.parse(json_string)
   end
-
-
 end
--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -1,2 +1,28 @@
 module PostHelper
+
+  def set_post_info(post)
+    @slug = "p" #default is p when not in a collection
+    @collection_name = ""
+    @author_name = "unknown"
+    @author_image = "unknown"
+
+    # set info for collection post is in
+    @collections.each do |collection|  
+      if collection[0] == post["homeCollectionId"]
+        @slug            = collection[1]["slug"]
+        @collection_name = collection[1]["name"]
+        break
+      end
+    end
+
+    # get author info
+    @authors.each do |author| 
+      if author[0] == post["creatorId"]
+        @author_name = author[1]["name"]
+        @author_image = author[1]["imageId"]
+        break
+      end
+    end
+  end
+
 end
